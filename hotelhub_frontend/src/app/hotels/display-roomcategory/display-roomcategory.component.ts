a2a402e79389703fba7e9283956c4d19e4ab24fe
KartikmPatel <kartikmpatel0804@gmail.com>
2024-10-29 16:59:35 +0530

    template set

--- hotelhub_frontend/src/app/hotels/display-roomcategory/display-roomcategory.component.ts
+++ hotelhub_frontend/src/app/hotels/display-roomcategory/display-roomcategory.component.ts
@@ -1,19 +1,18 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { RoomcategoryserviceService } from 'src/app/hotelservices/roomcategoryservice.service';
-
-declare var $: any;
+import { DataTable } from 'simple-datatables';
 
 @Component({
   selector: 'app-display-roomcategory',
   templateUrl: './display-roomcategory.component.html',
   styleUrls: ['./display-roomcategory.component.css']
 })
-export class DisplayRoomcategoryComponent {
+export class DisplayRoomcategoryComponent implements OnInit, AfterViewInit {
 
   categories: any[] = [];
-
+  
   constructor(private roomcategoryservice: RoomcategoryserviceService) { }
-
+  
   ngOnInit(): void {
     this.roomcategoryservice.getCategory().subscribe(data => {
       this.categories = data.$values;
@@ -21,9 +20,11 @@ export class DisplayRoomcategoryComponent {
       console.log(this.categories);
     });
   }
-
+  
+  // For Datatables
+  @ViewChild('dataTable', { static: true }) table!: ElementRef;
   ngAfterViewInit(): void {
-    // Initializes the DataTable after the view is initialized
-    $('#basic-datatables').DataTable({});
+    // Initialize Simple DataTable after view is initialized
+    new DataTable(this.table.nativeElement);
   }
 }
